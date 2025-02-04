1981cdcc0b84b3e69ea7b28f7ce805de5435cc0e
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-04 14:38:23 +0100

    basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""
-BasicCache module
+"""BasicCache module
 """
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-    inherits from BaseCaching and is a caching system
+    """inherits from BaseCaching and is a caching system
     """
 
     def put(self, key, item):
-        """assigns to the self.cache_data dictionary"""
+        """assigns to the self.cache_data dictionary
+        """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        returns the value in self.cache_data linked to key
+        """returns the value in self.cache_data linked to key
         """
         if key is None or key not in self.cache_data.keys():
             return None
