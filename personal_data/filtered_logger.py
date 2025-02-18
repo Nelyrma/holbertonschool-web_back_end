0327d9c1e19de9f56646a7eba7f39aadb091b53f
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-18 11:03:53 +0100

    log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """
 filtered logger module
 """
+import logging
 import re
 from typing import List, Tuple
 
@@ -21,3 +22,25 @@ def filter_datum(
         # replace the fields values with the redaction
         message = re.sub(pattern, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields : List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        filter values in incoming log records using filter_datum
+        """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+        )
+        return super().format(record)
