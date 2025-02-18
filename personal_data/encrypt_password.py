becf5461b57b20611f3ce066c24f56e14da74443
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-18 14:41:34 +0100

    Encrypting passwords

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""encrypt password module"""
+
+import bcrypt
+
+
+def hash_password(password) -> bytes:
+    """
+    returns a salted, hashed password, which is a byte string
+    """
+
+    # generate a salt
+    salt = bcrypt.gensalt()
+    # hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
