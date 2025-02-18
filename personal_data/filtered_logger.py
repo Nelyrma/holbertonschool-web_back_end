7291a9cef5e600b8c8020f592d1a79b457966951
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-18 10:11:13 +0100

    Obfuscate a log message

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,14 +3,21 @@
 filtered logger module
 """
 import re
+from typing import List, Tuple
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str
+) -> str:
     """
     returns the log message obfuscated
     """
-
-    # built a regex for fields
-    pattern = r'(' + '|'.join(fields) + rf'){separator}[^{separator}]+'
-    # replace the fields values with the redaction
-    return re.sub(pattern, rf'\1{separator}{redaction}', message)
+    for field in fields:
+        # built a regex for fields
+        pattern = r'(' + '|'.join(fields) + rf'){separator}[^{separator}]+'
+        # replace the fields values with the redaction
+        message = re.sub(pattern, rf'\1{separator}{redaction}', message)
+    return message
