e9b9d28d9317bd4b44bfae04df4410b380014c20
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-23 23:12:56 +0100

    fixing pycodestyle errors

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,13 +22,14 @@ class BasicAuth(Auth):
         return authorization_header[6:]
 
     def decode_base64_authorization_header(
-            self,base64_authorization_header: str) -> str:
+            self, base64_authorization_header: str) -> str:
         """ Returns the decoded value of base64_authorization_header """
         if (base64_authorization_header is None or
                 type(base64_authorization_header) is not str):
             return None
 
         try:
-            return base64.b64decode(base64_authorization_header).decode('utf-8')
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
         except Exception:
             return None
