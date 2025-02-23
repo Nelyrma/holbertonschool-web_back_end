4e610b6a43a447c9e11f8db7df035615357736b5
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-23 23:25:09 +0100

    user credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,14 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+        
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Returns the user email and password from the Base64 decoded value """
+        if (decoded_base64_authorization_header is None or
+                type(decoded_base64_authorization_header) is not str or
+                ':' not in decoded_base64_authorization_header):
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
