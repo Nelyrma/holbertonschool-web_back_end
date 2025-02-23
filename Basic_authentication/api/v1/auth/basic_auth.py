2c9c9336383d14863c8b7b62c4bbc6b27ceca662
Marlyne Ihorimbere <113862900+Nelyrma@users.noreply.github.com>
2025-02-23 22:52:38 +0100

    base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,18 @@ from models.user import User
 
 class BasicAuth(Auth):
     """ Basic authentification class """
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str) -> str:
+        """ Returns the decoded value of base64_authorization_header """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            # Decode the Base64 string
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            # Convert bytes to UTF-8 string
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except Exception:
+            return None
