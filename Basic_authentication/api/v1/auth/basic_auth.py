31c6f64e7065821c00bb090e1065afec2ec9cf3f
Marlyne Ihorimbere <113862900+Nelyrma@users.noreply.github.com>
2025-02-23 22:31:31 +0100

    basic auth class

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+""" Module of basic authentification """
+from typing import TypeVar
+import base64
+from api.v1.auth.auth import Auth
+from models.user import User
 
+
+class BasicAuth(Auth):
+    """ Basic authentification class """
