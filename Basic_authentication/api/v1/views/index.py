dd68d3ac58b91a77714a22b6e6638a9a13d1ee3d
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-23 21:45:15 +0100

    Adding 'Forbidden' error handler

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized():
     """ Endpoint to raise a 401 error """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ Endpoint to raise a 403 error """
+    abort(403)
