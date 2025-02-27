aba2f4735be9c0ad8288e529dda713cd4b440637
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-27 15:09:14 +0100

    Implement the add_user method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -18,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,15 +34,9 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
+        """Add a user to the database
         """
-        # Create a new user
         new_user = User(email=email, hashed_password=hashed_password)
-
-        # Add the user to the session
         self._session.add(new_user)
-
-        # save the modifications to the database
         self._session.commit()
-
         return new_user
