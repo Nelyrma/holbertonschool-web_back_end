8391fed912d82d5420ccd5a39a3b54aade430bf3
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-02-28 16:41:12 +0100

    update a user's attribute

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -58,3 +58,14 @@ class DB:
             return user
         except InvalidRequestError:
             raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user's attributes
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"Invalid attribute: {key}")
+            setattr(user, key, value)
+
+        self._session.commit()
