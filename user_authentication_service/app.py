166f331b33ac80497207f6871350610d055dd0f8
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-03-02 22:41:45 +0100

    Implement an end-point to register a user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -16,13 +16,13 @@ def welcome():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'])
+@app.route('/users', methods=["POST"])
 def users():
     """implement the POST /users route
     """
     # exctract form data
-    email = request.form.get('email')
-    password = request.form.get('password')
+    email = request.form.get("email")
+    password = request.form.get("password")
 
     # check that email and password fields are present
     if not email or not password:
@@ -31,7 +31,7 @@ def users():
     try:
         # register a new user
         new_user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"}), 201
+        return jsonify({"email": new_user.email, "message": "user created"}), 200
     except ValueError as e:
         # case where the email is already registered
         return jsonify({"message": "email already registered"}), 400
