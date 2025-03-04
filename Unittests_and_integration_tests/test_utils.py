dc4bce25a11edeebd151d5473445ce90ceae02f3
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-03-04 13:35:30 +0100

    test the memoize function

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -50,3 +50,23 @@ class TestGetJson(unittest.TestCase):
                    ) as mock_get:
             self.assertEqual(get_json(test_url), test_payload)
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize class"""
+    def test_memoize(self):
+        """Test that memoization caches method results."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+            
+        my_object = TestClass()
+        with patch.object(my_obj, 'a_method') as mock_method:
+            mock_method.return_value = 42
+            self.assertEqual(my_object.a_property, 42)
+            self.assertEqual(my_object.a_property, 42)
+            mock_method.assert_called_once()
