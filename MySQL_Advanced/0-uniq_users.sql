45b8676396fbcdc6fce64fb9e2125da93a1edc46
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-03-19 12:01:50 +0100

    Create a table users with attributes

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- script that creates the table users
 -- create the table users
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY_KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY_KEY (id)
 );
