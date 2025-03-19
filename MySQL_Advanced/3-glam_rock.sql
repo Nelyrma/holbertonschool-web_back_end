31efd0f1313e1b992c69c0bd19afcb75aecf6280
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-03-19 13:37:34 +0100

    Write a script that lists all bands with a certain style

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,12 @@
 -- Script that lists all bands with Glam rock
 -- as their main style, ranked by their longevity
 -- Select statement
-SELECT band_name, (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
-FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
+SELECT
+    band_name,
+    (IFNULL(split, 2024) - formed) AS LIFESPAN
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY
+    LIFESPAN DESC;
