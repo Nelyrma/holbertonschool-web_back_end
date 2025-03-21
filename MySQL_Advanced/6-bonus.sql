f1a8f52ba895ec56e8c8fda1e6b6420384d940bf
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-03-21 16:44:05 +0100

    Create a stored procedure that adds a new correction for a student

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -5,19 +5,20 @@ DELIMITER //
 
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-    DECLARE project_id INT,
+    DECLARE project_id INT;
 
-    -- check if the project already exists
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    -- Check if the project already exists
+    SELECT id INTO project_id FROM projects WHERE name = project_name LIMIT 1;
 
-    -- create the project if it doesn't exist
+    -- If the project doesn't exist, create it
     IF project_id IS NULL THEN
         INSERT INTO projects (name) VALUES (project_name);
         SET project_id = LAST_INSERT_ID();
     END IF;
 
-    -- add a new correction
+    -- Add a new correction
     INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END;//
+END;
+//
 
 DELIMITER ;
