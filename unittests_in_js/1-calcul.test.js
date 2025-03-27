248c0c9f79b020c64133f1b7b1d1fe45c5adab0b
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-03-27 15:46:18 +0100

    Combine descriptions

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -3,56 +3,36 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function() {
   describe('SUM', function() {
-    it('Checks 1 + 3', function() {
-      assert.equal(calculateNumber('SUM', 1, 3), 4);
+    it('should return 6 when inputs are SUM, 1.4, and 4.5', function() {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     });
-    it('Checks 1 + 3.7', function() {
-      assert.equal(calculateNumber('SUM', 1, 3.7), 5);
-    });
-    it('Checks 1.2 + 3.7', function() {
-      assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
-    });
-    it('Checks 1.4 + 4.5', function() {
-      assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
-    });
-    it('Checks -3 + 1.2', function() {
-      assert.equal(calculateNumber('SUM', -3, 1.2), -2);
+
+    it('should return 5 when inputs are SUM, 2.5, and 2.5', function() {
+      assert.strictEqual(calculateNumber('SUM', 2.5, 2.5), 5);
     });
   });
 
   describe('SUBTRACT', function() {
-    it('Checks 3 - 1', function() {
-      assert.equal(calculateNumber('SUBTRACT', 3, 1), 2);
+    it('should return -4 when inputs are SUBTRACT, 1.4, and 4.5', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
-    it('Checks 1.4 - 4.5', function() {
-      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-    });
-    it('Checks 1.2 - 3.7', function() {
-      assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
-    });
-    it('Checks 3.7 - 1.5', function() {
-      assert.equal(calculateNumber('SUBTRACT', 3.7, 1.5), 2);
-    });
-    it('Checks 0.1 - 0.1', function() {
-      assert.equal(calculateNumber('SUBTRACT', 0.1, 0.1), 0);
+
+    it('should return 0 when inputs are SUBTRACT, 2.5, and 2.5', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2.5, 2.5), 0);
     });
   });
 
   describe('DIVIDE', function() {
-    it('Checks 3 / 1', function() {
-      assert.equal(calculateNumber('DIVIDE', 3, 1), 3);
+    it('should return 0.2 when inputs are DIVIDE, 1.4, and 4.5', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
-    it('Checks 3.5 / 2.1', function() {
-      assert.equal(calculateNumber('DIVIDE', 3.5, 2.1), 2);
+
+    it('should return Error when inputs are DIVIDE, 1.4, and 0', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
-    it('Checks 1.4 / 4.5', function() {
-      assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    });
-    it('Checks 1.4 / 0', function() {
-      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    });
-    it('Checks 0.1 / 0.1', function() {
-      assert.equal(calculateNumber('DIVIDE', 0.1, 0.1), 1);
+
+    it('should return 1 when inputs are DIVIDE, 5, and 5', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 5, 5), 1);
     });
   });
 });
